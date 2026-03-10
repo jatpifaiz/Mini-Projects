aaf7869435077c7957404a7bd970ee3f8c3dbef9
jatpi faiz <jatpifaiz.26@gmail.com>
2026-03-10 17:22:00 +0700

    add mini project ngopi doloe

---
+++ ngopi-doloe/js/main.js
@@ -0,0 +1,17 @@
+// toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika humberger menu di klik
+document.querySelector('#hamburgerMenu').onclick = ( ) => {
+    navbarNav.classList.toggle('active')
+}
+
+// klik diluar sidebar untuk menghilangkan nav
+
+const hamburger = document.querySelector('#hamburgerMenu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
